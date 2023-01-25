7f0deedb25218ad77d15c7faddaae00ebb0774c5
waffleweb <waffleweb.it@gmail.com>
2023-01-25 16:18:47 +0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, ()=> {
+  await app.listen('https://adorable-beijinho-78b208.netlify.app', ()=> {
     console.log('ok')
   });
 }
